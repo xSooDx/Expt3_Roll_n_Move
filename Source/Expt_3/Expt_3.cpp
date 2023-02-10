684bfdd5867ff3dd6dae5b8e47a8b8c89714b1b7
Saurabh Sood <u1423247@umail.utah.edu>
2023-02-09 20:03:00 -0700

    init

---
+++ Source/Expt_3/Expt_3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Expt_3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Expt_3, "Expt_3" );
---
+++ Source/Expt_3/Expt_3GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Expt_3GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXPT_3_API AExpt_3GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
